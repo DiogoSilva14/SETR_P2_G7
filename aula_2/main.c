68e1d9d27fd4ad61b4634c52d6f8098bfe4a17f9
Mario <Mrio Dias>
2022-03-15 10:09:52 +0000

    vAvg not working

--- aula_2/main.c
+++ aula_2/main.c
@@ -2,6 +2,7 @@
 #include "vAdd.h"
 #include <stdint.h>
 #include <stdio.h>
+#include "vAvg.h"
 
 void main(){
     uint32_t array[10];
@@ -15,5 +16,10 @@ void main(){
     printf("Sum = %d\n", sum);
 
 
+    for(int i=0; i < 10; i++){
+        printf("%d \n", array[i]);
+    }
+    
+    printf("Avg = %d \n", avg);
     return 0;
 }
