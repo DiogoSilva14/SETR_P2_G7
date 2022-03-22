2fadd51334f7654bf09d8bee2ff5fd109c0764dd
Mario <Mrio Dias>
2022-03-22 10:30:36 +0000

    mysag_avg done, not tested

--- aula_3/MySAG_lib.c
+++ aula_3/MySAG_lib.c
@@ -1,1 +1,13 @@
 #include "MySAG_lib.h"
+#include <stdbool.h>
+
+uint32_t mysag_avg(MySAG *mysag_array)
+{
+    uint32_t sum = 0,avg = 0;
+    for(uint16_t i = 0; i < mysag_array->size; i++)
+    {
+        sum += mysag_array->data[i];
+    }
+    avg = sum/mysag_array->size;
+    return avg;
+}
