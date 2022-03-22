ffceea09eeb987ae5885dec414e9200406c1d738
Diogo Silva <diogo.silva.pt14@gmail.com>
2022-03-22 09:42:03 +0000

    Initial commit for project MySAG

---
+++ aula_3/main.c
@@ -0,0 +1,16 @@
+/** @file main.c
+ * @brief main.c file brief description
+ *
+ * Main file for the MySAG Project
+ * 
+ * @date 15 March 2022
+ * @bug No known bugs.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+    printf("Hello world!\n");
+    return 0;
+}
