70eb29869bdd3a2ef435b21dab498b6f20ab0256
Diogo Silva <diogo.silva.pt14@gmail.com>
2022-03-22 10:18:15 +0000

    Init working

--- aula_3/MySAG_lib.c
+++ aula_3/MySAG_lib.c
@@ -1,1 +1,11 @@
 #include "MySAG_lib.h"
+
+int32_t MySAGInit(MySAG* mysag_array){
+    for(int i=0; i < MAX_SIZE; i++){
+        mysag_array->data[i] = 0;
+    }
+
+    mysag_array->size = 0;
+
+    return 0;
+}
