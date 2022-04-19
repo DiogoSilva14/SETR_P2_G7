1980ffc7ddc527686105675cc7f345d2ea2959b3
Diogo Silva <diogosilva14@ua.pt>
2022-04-19 09:35:43 +0100

    Updated makefile and cleaned test file

---
+++ aula_5/test/testcmdproc.c
@@ -0,0 +1,22 @@
+#include <unity.h>
+#include "cmdproc.h"
+
+void setUp(void)
+{
+	return;
+}
+void tearDown(void)
+{
+	return;
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+/*	
+	RUN_TEST(test_MyVectorLib_Find_NotThere);
+	RUN_TEST(test_MyVectorLib_Find_AreThere);
+	RUN_TEST(test_MyVectorLib_Len_RightSize);
+*/		
+	return UNITY_END();
+}
