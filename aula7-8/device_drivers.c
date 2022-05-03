48e070617d149241d789c3620e0386b2219bcc7f
Diogo Silva <diogosilva14@ua.pt>
2022-05-03 10:50:41 +0100

    cleanup

--- aula7-8/device_drivers.c
+++
@@ -1,8 +0,0 @@
-/** @file device_drivers.c
- * @brief
- *
- * Device drivers for vending machine project
- * 
- * @date 03 May 2022
- * @bug No known bugs.
- */
--- aula7-8/vending_machine.c
+++
@@ -1,8 +0,0 @@
-/** @file vending_machine.c
- * @brief vending_machine.c file brief description
- *
- * Vending machine state machine implementation file
- * 
- * @date 03 May 2022
- * @bug No known bugs.
- */
